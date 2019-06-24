08a78a272b6b58f3150e9df4b6c7ee05752a5e3a
Cyvern-Inc <52170268+Cyvern-Inc@users.noreply.github.com>
2019-06-24 11:47:11 -0400

    Intial Upload
    This contains the alpha version of the program.

---
+++ Rename Files/Form1.Designer.cs
@@ -0,0 +1,290 @@
+﻿namespace Rename_Files
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.delCharsTB = new System.Windows.Forms.TextBox();
+            this.delCharsCB = new System.Windows.Forms.CheckBox();
+            this.resultTB1 = new System.Windows.Forms.TextBox();
+            this.resultTB2 = new System.Windows.Forms.TextBox();
+            this.addCharsCB = new System.Windows.Forms.CheckBox();
+            this.addCharsTB = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.endRB = new System.Windows.Forms.RadioButton();
+            this.startRB = new System.Windows.Forms.RadioButton();
+            this.executeBtn = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.progressText = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Silver;
+            this.label1.Location = new System.Drawing.Point(22, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(166, 26);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Select Directory";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(156, 50);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(405, 30);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Click += new System.EventHandler(this.textBox1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.HorizontalScrollbar = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(581, 81);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(352, 524);
+            this.listBox1.TabIndex = 2;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Location = new System.Drawing.Point(581, 50);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(352, 31);
+            this.panel1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(115, 3);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(120, 25);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Change Log";
+            // 
+            // delCharsTB
+            // 
+            this.delCharsTB.Location = new System.Drawing.Point(27, 165);
+            this.delCharsTB.Name = "delCharsTB";
+            this.delCharsTB.Size = new System.Drawing.Size(534, 30);
+            this.delCharsTB.TabIndex = 6;
+            this.delCharsTB.Enter += new System.EventHandler(this.delCharsTB_Enter);
+            // 
+            // delCharsCB
+            // 
+            this.delCharsCB.AutoSize = true;
+            this.delCharsCB.Location = new System.Drawing.Point(27, 110);
+            this.delCharsCB.Name = "delCharsCB";
+            this.delCharsCB.Size = new System.Drawing.Size(336, 29);
+            this.delCharsCB.TabIndex = 7;
+            this.delCharsCB.Text = "Delete characters from file name(s)";
+            this.delCharsCB.UseVisualStyleBackColor = true;
+            this.delCharsCB.CheckedChanged += new System.EventHandler(this.delCharsCB_CheckedChanged);
+            // 
+            // resultTB1
+            // 
+            this.resultTB1.Location = new System.Drawing.Point(27, 222);
+            this.resultTB1.Name = "resultTB1";
+            this.resultTB1.ReadOnly = true;
+            this.resultTB1.Size = new System.Drawing.Size(534, 30);
+            this.resultTB1.TabIndex = 8;
+            // 
+            // resultTB2
+            // 
+            this.resultTB2.Location = new System.Drawing.Point(27, 404);
+            this.resultTB2.Name = "resultTB2";
+            this.resultTB2.ReadOnly = true;
+            this.resultTB2.Size = new System.Drawing.Size(534, 30);
+            this.resultTB2.TabIndex = 11;
+            // 
+            // addCharsCB
+            // 
+            this.addCharsCB.AutoSize = true;
+            this.addCharsCB.Location = new System.Drawing.Point(27, 292);
+            this.addCharsCB.Name = "addCharsCB";
+            this.addCharsCB.Size = new System.Drawing.Size(294, 29);
+            this.addCharsCB.TabIndex = 10;
+            this.addCharsCB.Text = "Add characters to file name(s)";
+            this.addCharsCB.UseVisualStyleBackColor = true;
+            this.addCharsCB.CheckedChanged += new System.EventHandler(this.addCharsCB_CheckedChanged);
+            // 
+            // addCharsTB
+            // 
+            this.addCharsTB.Location = new System.Drawing.Point(27, 347);
+            this.addCharsTB.Name = "addCharsTB";
+            this.addCharsTB.Size = new System.Drawing.Size(534, 30);
+            this.addCharsTB.TabIndex = 9;
+            this.addCharsTB.Enter += new System.EventHandler(this.addCharsTB_Enter);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(42, 467);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(427, 25);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Add characters to the start or end of file name(s)";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.groupBox1.Controls.Add(this.endRB);
+            this.groupBox1.Controls.Add(this.startRB);
+            this.groupBox1.Location = new System.Drawing.Point(475, 440);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(86, 101);
+            this.groupBox1.TabIndex = 13;
+            this.groupBox1.TabStop = false;
+            // 
+            // endRB
+            // 
+            this.endRB.AutoSize = true;
+            this.endRB.Location = new System.Drawing.Point(6, 63);
+            this.endRB.Name = "endRB";
+            this.endRB.Size = new System.Drawing.Size(68, 29);
+            this.endRB.TabIndex = 1;
+            this.endRB.TabStop = true;
+            this.endRB.Text = "End";
+            this.endRB.UseVisualStyleBackColor = true;
+            // 
+            // startRB
+            // 
+            this.startRB.AutoSize = true;
+            this.startRB.Location = new System.Drawing.Point(6, 24);
+            this.startRB.Name = "startRB";
+            this.startRB.Size = new System.Drawing.Size(74, 29);
+            this.startRB.TabIndex = 0;
+            this.startRB.TabStop = true;
+            this.startRB.Text = "Start";
+            this.startRB.UseVisualStyleBackColor = true;
+            // 
+            // executeBtn
+            // 
+            this.executeBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.executeBtn.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.executeBtn.Location = new System.Drawing.Point(47, 566);
+            this.executeBtn.Name = "executeBtn";
+            this.executeBtn.Size = new System.Drawing.Size(168, 44);
+            this.executeBtn.TabIndex = 14;
+            this.executeBtn.Text = "Execute Batch";
+            this.executeBtn.UseVisualStyleBackColor = false;
+            this.executeBtn.Click += new System.EventHandler(this.executeBtn_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(237, 587);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(324, 23);
+            this.progressBar1.TabIndex = 15;
+            // 
+            // progressText
+            // 
+            this.progressText.AutoSize = true;
+            this.progressText.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.progressText.Location = new System.Drawing.Point(282, 564);
+            this.progressText.Name = "progressText";
+            this.progressText.Size = new System.Drawing.Size(223, 20);
+            this.progressText.TabIndex = 16;
+            this.progressText.Text = "Progress (0/0 files renamed)";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(949, 627);
+            this.Controls.Add(this.progressText);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.executeBtn);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.resultTB2);
+            this.Controls.Add(this.addCharsCB);
+            this.Controls.Add(this.addCharsTB);
+            this.Controls.Add(this.resultTB1);
+            this.Controls.Add(this.delCharsCB);
+            this.Controls.Add(this.delCharsTB);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Rename Files";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox delCharsTB;
+        private System.Windows.Forms.CheckBox delCharsCB;
+        private System.Windows.Forms.TextBox resultTB1;
+        private System.Windows.Forms.TextBox resultTB2;
+        private System.Windows.Forms.CheckBox addCharsCB;
+        private System.Windows.Forms.TextBox addCharsTB;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton endRB;
+        private System.Windows.Forms.RadioButton startRB;
+        private System.Windows.Forms.Button executeBtn;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label progressText;
+    }
+}
+
---
+++ Rename Files/Form1.cs
@@ -0,0 +1,319 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Rename_Files
+{
+    public partial class Form1 : Form
+    {
+        private string path;
+        private List<string> history;
+        public Form1()
+        {
+            InitializeComponent();
+
+            // initialize path
+            path = "";
+            history = new List<string>();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            delCharsTB.Text = "Characters/phrases to be removed, seperated by commas, enter to confirm";
+            addCharsTB.Text = "Characters/phrases to be added, seperated by commas, enter to confirm";
+        }
+
+        private List<string> GetCharsList(string chars)
+        {
+            // create a string list
+            List<string> charsList = new List<string>();
+
+            // split characters string based on ,
+            string[] tokens = chars.Split(',');
+
+            // remove extra spaces from each char and add to list
+            foreach(string token in tokens)
+            {
+                string c = token.TrimStart();
+                c = c.TrimEnd();
+
+                charsList.Add(c);
+            }
+
+            // return list
+            return charsList;
+        }
+
+        private void textBox1_Click(object sender, EventArgs e)
+        {
+            // create object of FolderBrowserDialog
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+
+            // show folder browser dialogue to select directory
+            DialogResult result = fbd.ShowDialog();
+
+            if(result == DialogResult.OK)
+            {
+                // get selected path
+                path = fbd.SelectedPath;
+
+                // display current path
+                textBox1.Text = path;
+            }
+        }
+
+        private void executeBtn_Click(object sender, EventArgs e)
+        {
+            if(string.IsNullOrEmpty(textBox1.Text))
+            {
+                MessageBox.Show("Please select directory!", "Message", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+
+            // if remove characters check box is checked
+            if (delCharsCB.Checked)
+            {
+                // get characters list to be removed
+                List<string> charList = GetCharsList(delCharsTB.Text);
+
+                // get files names
+                string[] fileNames = Directory.GetFiles(path + "\\");
+
+                // total files
+                int totalFiles = fileNames.Length;
+                // number of renamed files
+                int numFilesRenamed = 0;
+                // number of processed files
+                int numFilesProcessed = 0;
+
+                // set progress bar data
+                progressBar1.Minimum = 0;
+                progressBar1.Maximum = totalFiles;
+                progressText.Text = "Progress (" + numFilesProcessed + "/" + totalFiles + " files renamed)";
+                
+                // traverse each file
+                for (int i=0;i<fileNames.Length;i++)
+                {
+                    // get name
+                    string name = fileNames[i];
+
+                    // get file info
+                    FileInfo info = new FileInfo(name);
+
+                    // get Absolute name of files
+                    string name_absolute = info.Name;
+
+                    // set status of currently processing file
+                    resultTB1.Text = name_absolute;
+
+                    // set progress value
+                    progressBar1.Value = numFilesProcessed;
+
+                    // renamed name
+                    string new_name = name_absolute;
+
+                    // check file name for each character
+                    // if character exists in name then remove it
+                    foreach(string ch in charList)
+                    {
+                        // if file name contains character ch
+                        if(name_absolute.Contains(ch))
+                        {
+                            // remove character
+                            new_name = name_absolute.Replace(ch, "");
+                            // rename file
+                            info.MoveTo(info.Directory.FullName + "\\" + new_name);
+
+                            // update absolute name
+                            name_absolute = new_name;
+
+                            numFilesRenamed++;
+                        }
+                    }
+
+                    // add to history
+                    history.Add(new FileInfo(name).Name + " --> " + new_name);
+
+                    // increase renamed file number by 1
+                    numFilesProcessed++;
+
+                    progressText.Text = "Progress (" + numFilesProcessed + "/" + totalFiles + " files renamed)";
+                }
+
+                progressBar1.Value = progressBar1.Value + 1;
+
+                // clear list box items
+                listBox1.Items.Clear();
+                // add history to list box
+                foreach(string s in history)
+                {
+                    listBox1.Items.Add(s);
+                }
+            }
+
+            // if add characters check box is selected
+            if (addCharsCB.Checked)
+            {
+                if(!(startRB.Checked || endRB.Checked))
+                {
+                    MessageBox.Show("Please select any of mode to add character (Start/End)!", "Message", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    return;
+                }
+
+                // get characters list to be added
+                List<string> charList = GetCharsList(addCharsTB.Text);
+
+                // get files names
+                string[] fileNames = Directory.GetFiles(path + "\\");
+
+                // total files
+                int totalFiles = fileNames.Length;
+                // number of renamed files
+                int numFilesRenamed = 0;
+                // number of processed files
+                int numFilesProcessed = 0;
+
+                // set progress bar data
+                progressBar1.Minimum = 0;
+                progressBar1.Maximum = totalFiles;
+                progressText.Text = "Progress (" + numFilesProcessed + "/" + totalFiles + " files renamed)";
+
+                // traverse each file
+                foreach (string name in fileNames)
+                {
+                    // get file info
+                    FileInfo info = new FileInfo(name);
+
+                    // get Absolute name of files
+                    string name_absolute = info.Name;
+
+                    // set status of currently processing file
+                    resultTB2.Text = name_absolute;
+
+                    // set progress value
+                    progressBar1.Value = numFilesProcessed;
+
+                    // renamed name
+                    string new_name = name_absolute;
+
+                    // combine characters
+                    string chars = "";
+                    foreach (string c in charList)
+                    {
+                        chars += c;
+                    }
+
+                    // if start is elected
+                    if (startRB.Checked)
+                    {
+                        // add chars at start
+                        new_name = chars + name_absolute;
+                        // rename file
+                        info.MoveTo(info.Directory.FullName + "\\" + new_name);
+
+                        // add to history
+                        history.Add(name_absolute + " --> " + new_name);
+
+                        numFilesRenamed++;
+                    }
+                    else if (endRB.Checked)
+                    {
+                        // get last index of . to preserve file extension
+                        int index = -1;
+                        for (int i = 0; i < new_name.Length; i++)
+                        {
+                            if (new_name[i] == '.')
+                            {
+                                index = i;
+                            }
+                        }
+
+                        // extension length including .
+                        int ext_len = name_absolute.Length - index + 1;
+
+                        // get name without extension
+                        new_name = name_absolute.Substring(0, name_absolute.Length - ext_len);
+                        // get extension
+                        string ext = name_absolute.Substring(index, ext_len-1);
+
+                        // append chars at end
+                        new_name = new_name + chars;
+                        // append file extension
+                        new_name += ext;
+
+                        // rename file
+                        info.MoveTo(info.Directory.FullName + "\\" + new_name);
+
+                        // add to history
+                        history.Add(name_absolute + " --> " + new_name);
+
+                        numFilesRenamed++;
+                    }
+
+
+                    // increase renamed file number by 1
+                    numFilesProcessed++;
+
+                    progressText.Text = "Progress (" + numFilesProcessed + "/" + totalFiles + " files renamed)";
+                }
+
+                progressBar1.Value = progressBar1.Value + 1;
+
+                // clear list box items
+                listBox1.Items.Clear();
+                // add history to list box
+                foreach (string s in history)
+                {
+                    listBox1.Items.Add(s);
+                }
+            }
+
+            // show error message if no check box is selected
+            if (!(delCharsCB.Checked || addCharsCB.Checked))
+            {
+                MessageBox.Show("Please select any of the modes to rename files!", "Message", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void delCharsCB_CheckedChanged(object sender, EventArgs e)
+        {
+            delCharsTB.Text = "Characters/phrases to be removed, seperated by commas, enter to confirm";
+            progressBar1.Value = 0;
+            progressText.Text = "Progress (0/0 files renamed)";
+            resultTB1.Text = "";
+        }
+
+        private void addCharsCB_CheckedChanged(object sender, EventArgs e)
+        {
+            addCharsTB.Text = "Characters/phrases to be added, seperated by commas, enter to confirm";
+            resultTB2.Text = "";
+            progressBar1.Value = 0;
+            progressText.Text = "Progress (0/0 files renamed)";
+            startRB.Checked = false;
+            endRB.Checked = false;
+        }
+
+        private void delCharsTB_Enter(object sender, EventArgs e)
+        {
+            ToolTip tt = new ToolTip();
+            tt.Show("Characters/phrases to be removed, seperated by commas, enter to confirm", delCharsTB, 10, -20, 3000);
+
+            if (delCharsTB.Text.Equals("Characters/phrases to be removed, seperated by commas, enter to confirm"))
+            {
+                delCharsTB.Text = "";
+            }
+        }
+
+        private void addCharsTB_Enter(object sender, EventArgs e)
+        {
+            ToolTip tt = new ToolTip();
+            tt.Show("Characters/phrases to be add, seperated by commas, enter to confirm", addCharsTB, 10, -20, 3000);
+
+            if (addCharsTB.Text.Equals("Characters/phrases to be added, seperated by commas, enter to confirm"))
+            {
+                addCharsTB.Text = "";
+            }
+        }
+    }
+}
